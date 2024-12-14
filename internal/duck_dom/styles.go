f077936d8a7975fd91c93e2d28d8ea74bbd3dae3
nodari-dev <nodari.pylypyshak@gmail.com>
2024-12-14 19:27:20 +0100

    we need more borders

--- internal/duck_dom/styles.go
+++ internal/duck_dom/styles.go
@@ -2,23 +2,75 @@ package duckdom
 
 // oh boy CSS is comming
 
-type Styles struct{
-	Width int
-	Height int
-	Paddding int
-	Maaargin int
+type Styles struct {
+	Width      int
+	Height     int
+	Paddding   int
+	Maaargin   int
 	Background string
-	Border Border
+	Border     Border
 }
 
 type BorderStyle int
+
 const (
 	Solid BorderStyle = iota
-	Dashed
-
+	Bold
 )
-type Border struct{
+
+type Border struct {
 	Width int
 	Style BorderStyle
 	Color string
 }
+
+type BorderParts struct {
+	Top          string
+	Bottom       string
+	Left         string
+	Right        string
+	TopLeft      string
+	TopRight     string
+	BottomLeft   string
+	BottomRight  string
+	MiddleLeft   string
+	MiddleRight  string
+	Middle       string
+	MiddleTop    string
+	MiddleBottom string
+}
+
+var (
+	normalBorder = BorderParts{
+		Top:          "─",
+		Bottom:       "─",
+		Left:         "│",
+		Right:        "│",
+		TopLeft:      "┌",
+		TopRight:     "┐",
+		BottomLeft:   "└",
+		BottomRight:  "┘",
+		MiddleLeft:   "├",
+		MiddleRight:  "┤",
+		Middle:       "┼",
+		MiddleTop:    "┬",
+		MiddleBottom: "┴",
+	}
+
+	boldBorder = BorderParts{
+		Top:          "━",
+		Bottom:       "━",
+		Left:         "┃",
+		Right:        "┃",
+		TopLeft:      "┏",
+		TopRight:     "┓",
+		BottomLeft:   "┗",
+		BottomRight:  "┛",
+		MiddleLeft:   "┣",
+		MiddleRight:  "┫",
+		Middle:       "╋",
+		MiddleTop:    "┳",
+		MiddleBottom: "┻",
+	}
+)
+
--- internal/duck_dom/window.go
+++ internal/duck_dom/window.go
@@ -30,23 +30,66 @@ func (self *Window) SetBackground(b string) Stylable{
 
 func (self *Window) Render() string {
 	var string_builder strings.Builder
+
+	if(self.Styles.Border.Width != 0){
+		return render_with_border(self, &string_builder)
+	} 
+
+	return render_default(self, &string_builder)
+}
+
+func render_default(self *Window, string_builder *strings.Builder) string{
 	string_builder.WriteString(self.Styles.Background)
-	fillament := ""
-	horizontal_border_filament := strings.Repeat("-", self.Styles.Width) 
-	vertical_border_filament := "|" + strings.Repeat(" ", self.Styles.Width - 2) + "|"
+	fillament := strings.Repeat(" ", self.Styles.Width)
 
-	for row := self.Pos.StartingRow; row < uint(self.Styles.Height); row+= 1{
-		if(row == self.Pos.StartingRow || row == uint(self.Styles.Height - 1)){
-			fillament = horizontal_border_filament
-		} else{
-			fillament = vertical_border_filament
-		}
-
+	for row := self.Pos.StartingRow; row < uint(self.Styles.Height); row += 1{
 		string_builder.WriteString(fmt.Sprintf(MOVE_CURSOR_TO_POSITION, row, self.Pos.StartingCol))
 		string_builder.WriteString(fillament)
 	}
 	string_builder.WriteString(RESET_STYLES)
 
+	return string_builder.String()
+}
+
+func render_with_border(self *Window, string_builder *strings.Builder) string{
+	string_builder.WriteString(fmt.Sprintf(MOVE_CURSOR_TO_POSITION, self.Pos.StartingRow, self.Pos.StartingCol))
+	
+	// top border
+	var top_bor_builder strings.Builder
+	top_bor_builder.WriteString(self.Styles.Border.Color)
+	top_bor_builder.WriteString(normalBorder.TopLeft)
+	top_bor_builder.WriteString(strings.Repeat(normalBorder.Top, self.Styles.Width - 2))
+	top_bor_builder.WriteString(normalBorder.TopRight)
+
+	// bottom border
+	var bot_bor_builder strings.Builder
+	bot_bor_builder.WriteString(normalBorder.BottomLeft)
+	bot_bor_builder.WriteString(strings.Repeat(normalBorder.Bottom, self.Styles.Width - 2))
+	bot_bor_builder.WriteString(normalBorder.BottomRight)
+
+	// general filament 
+	var ver_bor_filament strings.Builder
+	ver_bor_filament.WriteString(self.Styles.Border.Color)
+	ver_bor_filament.WriteString(normalBorder.Left)
+	ver_bor_filament.WriteString(self.Styles.Background)
+	ver_bor_filament.WriteString(strings.Repeat(" ", self.Styles.Width - 2))
+	ver_bor_filament.WriteString(self.Styles.Border.Color)
+	ver_bor_filament.WriteString(normalBorder.Right)
+
+	string_builder.WriteString(top_bor_builder.String())
+	string_builder.WriteString(self.Styles.Background)
+
+	for row := self.Pos.StartingRow + 1; row < uint(self.Styles.Height) - 1; row += 1{
+		string_builder.WriteString(fmt.Sprintf(MOVE_CURSOR_TO_POSITION, row, self.Pos.StartingCol))
+		string_builder.WriteString(ver_bor_filament.String())
+	}
+
+	string_builder.WriteString(fmt.Sprintf(MOVE_CURSOR_TO_POSITION, self.Styles.Height - 1, self.Pos.StartingCol))
+	string_builder.WriteString(self.Styles.Border.Color)
+	string_builder.WriteString(bot_bor_builder.String())
+	string_builder.WriteString(RESET_STYLES)
+
+	// FIXME: GREY BIRDER
 	return string_builder.String()
 }
 
