38f1ad784be2a969d72930d372831e89c7f95dae
nodari-dev <nodari.pylypyshak@gmail.com>
2024-12-22 21:55:34 +0100

    pointers are here.

--- main.go
+++ main.go
@@ -129,7 +129,6 @@ func main() {
 
 	sidebar := dd.Window{
 		Position: dd.Position{StartingRow: 1, StartingCol: 1},
-		Screen: &screen,
 		Styles: dd.Styles{
 			Width:      50,
 			Height:     screen.Height - 1,
@@ -138,9 +137,34 @@ func main() {
 		},
 	}
 
+	sidebar.AddChild(
+		&dd.Component{
+			Position: dd.Position{StartingRow: 3, StartingCol: uint(sidebar.StartingCol) + 2},
+			Buffer:   "|Deez nuts|",
+			Styles: dd.Styles{
+				Width:      screen.Width - sidebar.Styles.Width - 1,
+				Height:     screen.Height,
+				Background: dd.MakeRGBBackground(80, 40, 100),
+				// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
+			},
+		})
+
+	sidebar.AddChild(
+		&dd.Component{
+			Position: dd.Position{StartingRow: 5, StartingCol: uint(sidebar.StartingCol) + 2},
+			Buffer:   "|got em|",
+			Styles: dd.Styles{
+				Width:      screen.Width - sidebar.Styles.Width - 1,
+				Height:     screen.Height,
+				Background: dd.MakeRGBBackground(80, 40, 100),
+				// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
+			},
+		})
+
+	screen.AddChild(&sidebar)
+
 	content := dd.Window{
 		Position: dd.Position{StartingRow: 1, StartingCol: uint(sidebar.Styles.Width) + 1},
-		Screen: &screen,
 		Styles: dd.Styles{
 			Width:      screen.Width - sidebar.Styles.Width - 1,
 			Height:     int(float32(screen.Height)*0.7) + 1,
@@ -148,10 +172,34 @@ func main() {
 			Border:     dd.Border{Style: dd.RoundedBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
 		},
 	}
+	content.AddChild(
+		&dd.Component{
+			Position: dd.Position{StartingRow: 2, StartingCol: uint(content.StartingCol) + 2},
+			Buffer:   "|SIMD|",
+			Styles: dd.Styles{
+				Width:      screen.Width - sidebar.Styles.Width - 1,
+				Height:     screen.Height,
+				Background: dd.MakeRGBBackground(80, 40, 100),
+				// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
+			},
+		})
+
+	content.AddChild(
+		&dd.Component{
+			Position: dd.Position{StartingRow: 4, StartingCol: uint(content.StartingCol) + 2},
+			Buffer:   "|Ligma?|",
+			Styles: dd.Styles{
+				Width:      screen.Width - sidebar.Styles.Width - 1,
+				Height:     screen.Height,
+				Background: dd.MakeRGBBackground(80, 40, 100),
+				// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
+			},
+		})
+
+	screen.AddChild(&content)
 
 	input_bar := dd.Window{
 		Position: dd.Position{StartingRow: uint(content.Height) + 1, StartingCol: uint(sidebar.Width) + 1},
-		Screen: &screen,
 		Styles: dd.Styles{
 			Width:      screen.Width - sidebar.Styles.Width - 1,
 			Height:     int(float32(screen.Height)*0.3) - 1,
@@ -159,14 +207,8 @@ func main() {
 			Border:     dd.Border{Style: dd.RoundedBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
 		},
 	}
-	status_bar_component := dd.Component{
-		Position: dd.Position{StartingRow: uint(screen.Height), StartingCol: 2},
-		Buffer:   dd.NORMAL,
-		Styles: dd.Styles{
-			Width:  screen.Width,
-			Height: 1,
-		},
-	}
+	
+	screen.AddChild(&input_bar)
 
 	screen.StatusBar = dd.Window{
 		Position: dd.Position{StartingRow: uint(screen.Height), StartingCol: 1},
@@ -175,62 +217,21 @@ func main() {
 			Height:     1,
 			Background: dd.MakeRGBBackground(80, 40, 100),
 		},
-		Components: []dd.Component{status_bar_component},
 	}
-
-	item := dd.Component{
-		Position: dd.Position{StartingRow: 3, StartingCol: uint(sidebar.StartingCol) + 2},
-		Buffer:   "|Deez nuts|",
-		Styles: dd.Styles{
-			Width:      screen.Width - sidebar.Styles.Width - 1,
-			Height:     screen.Height,
-			Background: dd.MakeRGBBackground(80, 40, 100),
-			// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
-		},
-	}
-
-	item_two := dd.Component{
-		Position: dd.Position{StartingRow: 5, StartingCol: uint(sidebar.StartingCol) + 2},
-		Buffer:   "|got em|",
-		Styles: dd.Styles{
-			Width:      screen.Width - sidebar.Styles.Width - 1,
-			Height:     screen.Height,
-			Background: dd.MakeRGBBackground(80, 40, 100),
-			// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
-		},
-	}
-
-	sidebar.Components = []dd.Component{item, item_two}
-
-	// item_three := dd.Component{
-	// 	Position: dd.Position{StartingRow: 2, StartingCol: uint(content.StartingCol) + 2},
-	// 	Buffer:   "|SIMD|",
-	// 	Styles: dd.Styles{
-	// 		Width:      screen.Width - sidebar.Styles.Width - 1,
-	// 		Height:     screen.Height,
-	// 		Background: dd.MakeRGBBackground(80, 40, 100),
-	// 		// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
-	// 	},
-	// }
-	//
-	// item_four := dd.Component{
-	// 	Position: dd.Position{StartingRow: 4, StartingCol: uint(content.StartingCol) + 2},
-	// 	Buffer:   "|Ligma?|",
-	// 	Styles: dd.Styles{
-	// 		Width:      screen.Width - sidebar.Styles.Width - 1,
-	// 		Height:     screen.Height,
-	// 		Background: dd.MakeRGBBackground(80, 40, 100),
-	// 		// Border:     dd.Border{Style: dd.BoldBorder, Color: dd.MakeRGBTextColor(100, 100, 100)},
-	// 	},
-	// }
-
-	// content.Components = []dd.Component{item_three, item_four}
-
-	screen.Windows = append(screen.Windows, sidebar, content, input_bar)
+	screen.StatusBar.AddChild(
+		&dd.Component{
+			Position: dd.Position{StartingRow: uint(screen.Height), StartingCol: 2},
+			Buffer:   dd.NORMAL,
+			Styles: dd.Styles{
+				Width:  screen.Width,
+				Height: 1,
+			},
+		})
 
 	screen.Activate()
 	screen.Render()
 
+	// TODO: Check if its possible to accept more than one byte
 	stdin_buffer := make([]byte, 1)
 	for screen.EventLoopIsRunning {
 		for len(screen.RenderQueue) > 0 {
