6fc624ee348f77d526feae8b23235f709dab05f3
salty <rslnkssk@gmail.com>
2025-03-23 16:59:01 +0100

    add raw mode

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,17 @@ const ENABLE_ECHO_INPUT: u32 = 0x4;
 const ENABLE_PROCESSED_INPUT: u32 = 0x1;
 const ENABLE_WINDOW_INPUT: u32 = 0x8;
 
-fn start_raw_mode(std_in: std.fs.File, std_out: std.fs.File) !void {
+const OldState = union {
+    win: struct {
+        std_out: *shit_os.DWORD,
+        std_in: *shit_os.DWORD,
+    },
+    posix: struct {
+        std_out: *posix.termios,
+        std_in: posix.termios,
+    },
+};
+fn start_raw_mode(std_in: std.fs.File, std_out: std.fs.File, termos: *OldState) !void {
     switch (os_tag) {
         .windows => {
             var old_stdin: shit_os.DWORD = undefined;
@@ -23,34 +33,51 @@ fn start_raw_mode(std_in: std.fs.File, std_out: std.fs.File) !void {
 
             var old_stdout: shit_os.DWORD = undefined;
             _ = shit_os.kernel32.GetConsoleMode(std_out, &old_stdout);
-            _ = shit_os.kernel32.SetConsoleMode(std_out, old_stdout | shit_os.ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+            _ = shit_os.kernel32.SetConsoleMode(std_out, termos.win.std_out | shit_os.ENABLE_VIRTUAL_TERMINAL_PROCESSING);
         },
-        .linux => {},
+        .linux => {
+            termos.posix.std_in = try posix.tcgetattr(std_in.handle);
+            var raw = termos.posix.std_in;
+            raw.lflag.ECHO = false;
+            raw.lflag.ICANON = false;
+            raw.lflag.ISIG = false;
+            raw.lflag.IEXTEN = false;
+            raw.iflag.ICRNL = false;
+            raw.iflag.IXON = false;
+            raw.oflag.OPOST = false;
+            try posix.tcsetattr(
+                std_in.handle,
+                .FLUSH,
+                raw,
+            );
+        },
         else => return error.UNSUPPORTED_OS,
     }
 }
 
-fn restore_terminal(std_in: std.fs.File, old_stdin: ?shit_os.DWORD) void {
+fn restore_terminal(std_in: std.fs.File, termos: OldState) void {
     switch (os_tag) {
         .windows => {
-            _ = shit_os.kernel32.SetConsoleMode(std_in, old_stdin.?);
+            _ = shit_os.kernel32.SetConsoleMode(std_in, termos.win.std_in.*);
         },
-        .linux => {},
+        .linux => {
+            posix.tcsetattr(
+                std_in.handle,
+                .FLUSH,
+                termos.posix.std_in,
+            ) catch unreachable;
+        },
         else => {},
     }
 }
 
 fn read_terminal(std_in: std.fs.File, stdout: std.fs.File.Writer) !bool {
     var buf = [1]u8{0};
-    switch (os_tag) {
-        .windows => _ = try shit_os.ReadFile(std_in, &buf, null),
-        .linux => {},
-        else => {},
-    }
+    _ = try std_in.read(&buf);
     switch (buf[0]) {
         3 => return true,
         '\n' => try stdout.print("\n", .{}),
-        else => try stdout.print("\x1b[48;2;25;60;80m{c}\x1b[0m", .{buf[0]}),
+        else => try stdout.print("\x1b[48;2;25;60;80m{c}\x1b[0m\n", .{buf[0]}),
     }
     return false;
 }
@@ -58,25 +85,20 @@ fn read_terminal(std_in: std.fs.File, stdout: std.fs.File.Writer) !bool {
 pub const TerminalDimensions = struct { width: u16, height: u16 };
 pub const UnixWinSize = struct { row: u16 = 0, col: u16 = 0, xpixel: u16 = 0, ypixel: u16 = 0 };
 
-fn get_terminal_dimentions(std_out: std.fs.File) !TerminalDimensions {
-    const terminal_dimensions: TerminalDimensions = undefined;
+fn get_terminal_dimensions(std_out: std.fs.File, terminal_dimensions: *TerminalDimensions) !void {
     switch (os_tag) {
         .windows => {
-            terminal_dimensions = .{
-                .width = shit_os.CONSOLE_SCREEN_BUFFER_INFO.dwSize.X,
-                .height = shit_os.CONSOLE_SCREEN_BUFFER_INFO.dwSize.Y,
-            };
+            terminal_dimensions.width = shit_os.CONSOLE_SCREEN_BUFFER_INFO.dwSize.X;
+            terminal_dimensions.height = shit_os.CONSOLE_SCREEN_BUFFER_INFO.dwSize.Y;
         },
         .linux, .macos => {
-            const win_size: UnixWinSize = .{};
-            const res = std.os.linux.ioctl(@intCast(std_out.handle), std.os.linux.T.IOCGWINSZ, @intFromPtr(&win_size));
+            var win_size: UnixWinSize = .{};
+            const res = std.os.linux.ioctl(std_out.handle, std.os.linux.T.IOCGWINSZ, @intFromPtr(&win_size));
             if (res != 0) {
                 return error.ioctl_return_error_during_getting_linux_dimentions;
             }
-            terminal_dimensions = .{
-                .width = win_size.col,
-                .height = win_size.row,
-            };
+            terminal_dimensions.width = win_size.col;
+            terminal_dimensions.height = win_size.row;
         },
         else => return error.UNSUPPORTED_OS,
     }
@@ -88,7 +110,22 @@ pub fn main() !void {
     const std_in = std.io.getStdIn();
     const stdout = std_out.writer();
 
-    get_terminal_dimentions(std_out);
+    var terminal_dimensions: TerminalDimensions = undefined;
+
+    try get_terminal_dimensions(
+        std_out,
+        &terminal_dimensions,
+    );
+    try stdout.print("{}", .{terminal_dimensions});
+    var termos = switch (os_tag) {
+        .windows => OldState{ .win = .{ .std_in = 0, .std_out = 0 } },
+        .linux, .macos => OldState{ .posix = .{ .std_in = undefined, .std_out = undefined } },
+        else => {
+            return error.UNSUPPORTED_OS;
+        },
+    };
+    try start_raw_mode(std_in, std_out, &termos);
+    defer restore_terminal(std_in, termos);
 
     var exit = false;
 
