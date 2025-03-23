a74d2d4995463667afca6559fc294edb39a22b4c
salty <rslnkssk@gmail.com>
2025-03-23 14:40:23 +0100

    continue erwrite

--- src/main.zig
+++ src/main.zig
@@ -29,38 +29,45 @@ fn start_raw_mode(std_in: std.io.File, std_out: std.io.File) !void {
     }
 }
 
-fn restore_terminal(std_in: std.io.File, std_out: std.io.File) void {
+fn restore_terminal(std_in: std.io.File, old_stdin: ?windows.DWORD) void {
     switch (os_tag) {
         .windows => {
-            _ = windows.kernel32.SetConsoleMode(std_in, old_stdin);
+            _ = windows.kernel32.SetConsoleMode(std_in, old_stdin.?);
         },
         .linux => {},
         else => {},
     }
 }
 
+fn read_terminal(std_in: std.io.File, stdout: std.io.File.Writer) bool {
+    var buf = [1]u8{0};
+    switch (os_tag) {
+        .windows => _ = try windows.ReadFile(std_in, &buf, null),
+        .linux => {},
+        else => {},
+    }
+    switch (buf[0]) {
+        3 => return true,
+        '\n' => try stdout.print("\n", .{}),
+        else => try stdout.print("\x1b[48;2;25;60;80m{c}\x1b[0m", .{buf[0]}),
+    }
+    return false;
+}
+
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const std_out = std.io.getStdOut();
     const std_in = std.io.getStdIn();
+    const stdout = std_out.writer();
 
-    if (@import("builtin").os.tag == .windows) {
+    // get console dimensions
+    // var console_info: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
 
-        // var console_info: windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
+    // _ = windows.kernel32.GetConsoleScreenBufferInfo(std_out, &console_info);
+    // try stdout.print("{}\n", .{console_info});
 
-        // _ = windows.kernel32.GetConsoleScreenBufferInfo(std_out, &console_info);
-        // try stdout.print("{}\n", .{console_info});
+    var exit = false;
 
-        var exit = false;
-
-        while (!exit) {
-            var buf = [1]u8{0};
-            _ = try windows.ReadFile(std_in, &buf, null);
-
-            switch (buf[0]) {
-                3 => exit = true,
-                '\n' => try stdout.print("\n", .{}),
-                else => try stdout.print("\x1b[48;2;25;60;80m{c}\x1b[0m", .{buf[0]}),
-            }
-        }
-    } else {}
+    while (!exit) {
+        exit = read_terminal(std_in, stdout);
+    }
 }
