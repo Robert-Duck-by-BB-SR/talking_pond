4cea5f1659b766f5dc595e579f04dd24094e35f6
salty <rslnkssk@gmail.com>
2025-04-16 11:14:22 +0200

    go: remove go from our system

--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/Robert-Duck-by-BB-SR/talking_pond
-
-go 1.22.2
-
-require golang.org/x/term v0.28.0
-
-require golang.org/x/sys v0.29.0 // indirect
--- internal/duck_dom/utils.go
+++
@@ -1,8 +0,0 @@
-package duckdom
-
-import "fmt"
-
-// in case we will need it later in our lifes
-func conv_perc_to_string(dyn_value int, perc float64) string {
-	return fmt.Sprint(int(float64(dyn_value) * perc))
-}
